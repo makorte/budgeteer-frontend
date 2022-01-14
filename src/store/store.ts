588248a381a4b0bc9520bda35a655589ba89801f
Maximilian Korte <maximilian.korte@adesso.de>
2022-01-14 17:14:11 +0100

    Refactor to usage of redux createReducer() and createAction()

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,27 +1,25 @@
 import Store from "../types/Store";
-import {ActionTypes, SET_CURRENT_PROJECT} from "./actions";
+import {SET_CURRENT_PROJECT} from "./actions";
 import {createStore} from "redux";
 import Project from "../types/Project";
+import {createAction, createReducer} from "@reduxjs/toolkit";
 
-export const setProject = (project: Project) => project
+const setProject = createAction<Project>(SET_CURRENT_PROJECT)
 
-const initialState = {
+const initialState: Store = {
     project: {
         id: "",
         name: ""
     }
-} as Store
-
-function projectReducer(state: Store = initialState, action: ActionTypes) {
-    switch(action.type) {
-        case SET_CURRENT_PROJECT:
-            return {
-                project: action.payload
-            }
-        default:
-            return state
-    }
 }
+
+const projectReducer = createReducer(initialState, builder => {
+    builder
+        .addCase(setProject, (state, action) => {
+            state.project = action.payload
+        })
+})
+
 
 const store = createStore(
     projectReducer,
