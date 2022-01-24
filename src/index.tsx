10479631a267f46b65955af6f2300b1ce432b3e6
Maximilian Korte <maximilian.korte@adesso.de>
2022-01-24 10:11:33 +0100

    Combine index.tsx and App.tsx into index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {BrowserRouter as Router} from "react-router-dom";
+import {Route, Routes, BrowserRouter as Router} from 'react-router-dom'
+import {Provider} from "react-redux";
+
+import RegisterPage from "./components/user/RegisterPage";
+import LoginPage from "./components/user/LoginPage";
+import LogoutPage from "./components/user/LogoutPage"
+import SelectProjectPage from "./components/project/SelectProjectPage";
+import DashboardPage from "./components/project/DashboardPage";
+import ContractsPage from "./components/contracts/ContractsPage";
+import CreateContractPage from "./components/contracts/CreateContractPage";
+
+import store from "./store/store"
+import ContractDetailsPage from "./components/contracts/ContractDetailsPage";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Router>
-            <App/>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<SelectProjectPage/>}/>
+                    <Route path="/login" element={<LoginPage/>}/>
+                    <Route path="/register" element={<RegisterPage/>}/>
+                    <Route path="/logout" element={<LogoutPage/>}/>
+                    <Route path="/selectProject" element={<SelectProjectPage/>}/>
+                    <Route path="/dashboard" element={<DashboardPage/>}/>
+                    <Route path="/contracts" element={<ContractsPage/>}/>
+                    <Route path="/contracts/create" element={<CreateContractPage/>}/>
+                    <Route path="/contracts/details" element={<ContractDetailsPage/>}/>
+                </Routes>
+            </Router>
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
-)
-;
+);
