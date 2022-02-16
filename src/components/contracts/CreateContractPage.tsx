92adc737d867ebfeeb19e183a33d05072857e3b0
Maximilian Korte <maximilian.korte@adesso.de>
2022-02-16 11:36:51 +0100

    Refactor CreateContractPage to react-hook-form

--- src/components/contracts/CreateContractPage.tsx
+++ src/components/contracts/CreateContractPage.tsx
@@ -1,48 +1,36 @@
 import {FIXED_PRICE, TIME_AND_MATERIAL} from "../../types/Contract";
-import {FormEvent, useEffect, useState} from "react";
+import {useEffect} from "react";
 import CreateContract from "../../types/CreateContract";
 import {useSelector} from "react-redux";
 import {Link, useNavigate} from "react-router-dom";
 import {AxiosError} from "axios";
 import {RootStore} from "../../store/store";
 import {createContract, updateContract} from "../../services/ContractService";
+import {SubmitHandler, useForm} from "react-hook-form";
 
 const CreateContractPage = () => {
     const currentContract = useSelector((state: RootStore) => state.contract.contract)
     const projectId = useSelector((state: RootStore) => state.project.project.id)
-    const [createdContract, setCreatedContract] = useState<CreateContract>({
-        budget: {amount: "0", currencyCode: "EUR"},
-        internalNumber: "",
-        name: "",
-        startDate: "",
-        taxRate: "0",
-        type: TIME_AND_MATERIAL
-    })
+    const {setValue, register, handleSubmit, formState: {errors}} = useForm<CreateContract>()
 
     const navigate = useNavigate()
 
     useEffect(() => {
         if (currentContract.id) {
-            setCreatedContract({
-                budget: currentContract.budget,
-                internalNumber: currentContract.internalNumber,
-                name: currentContract.name,
-                startDate: currentContract.startDate,
-                taxRate: currentContract.taxRate!.toString(),
-                type: currentContract.type
-            })
+            setValue("budget", currentContract.budget)
+            setValue("internalNumber", currentContract.internalNumber)
+            setValue("name", currentContract.name)
+            setValue("startDate", currentContract.startDate)
+            setValue("taxRate", currentContract.taxRate!.toString())
+            setValue("type", currentContract.startDate)
         }
     }, [currentContract])
 
-    const onCreateContract = (e: FormEvent) => {
-        e.preventDefault()
-
+    const onCreateContract: SubmitHandler<CreateContract> = data => {
         if (!projectId) {
             navigate("/selectProject")
-        } else if (createdContract.type === "" || createdContract.type === undefined) {
-            alert("Please select type!")
         } else if (currentContract.id) {
-            updateContract(currentContract.id, createdContract)
+            updateContract(currentContract.id, data)
                 .then(() => navigate("/contracts"))
                 .catch((err: AxiosError) => {
                     if (err.response?.status === 401) {
@@ -52,7 +40,7 @@ const CreateContractPage = () => {
                     }
                 })
         } else {
-            createContract(projectId!, createdContract)
+            createContract(projectId!, data)
                 .then(() => navigate("/contracts"))
                 .catch((err: AxiosError) => {
                     if (err.response?.status === 401) {
@@ -68,29 +56,32 @@ const CreateContractPage = () => {
         <div>
             <Link to={"/contracts"}>Back</Link>
             <h3>Create Contract</h3>
-            <form onSubmit={onCreateContract}>
+            <form onSubmit={handleSubmit(onCreateContract)}>
                 <label htmlFor={"name"}>Name</label><br/>
-                <input type={"text"} id={"name"}
-                       onChange={e => setCreatedContract({...createdContract, name: e.target.value})} value={createdContract.name}/><br/>
+                <input {...register("name", {
+                    required: "Please enter a name!"
+                })} type={"text"} id={"name"}/><br/>
                 <label htmlFor={"internalNumber"}>Id</label><br/>
-                <input type={"text"} id={"internalNumber"}
-                       onChange={e => setCreatedContract({...createdContract, internalNumber: e.target.value})} value={createdContract.internalNumber}/><br/>
+                <input {...register("internalNumber", {
+                    required: "Please enter an id!"
+                })} type={"text"} id={"internalNumber"}/><br/>
                 <label htmlFor={"startDate"}>Start date</label><br/>
-                <input type={"date"} id={"startDate"}
-                       onChange={e => setCreatedContract({...createdContract, startDate: e.target.value})} value={createdContract.startDate}/><br/>
+                <input {...register("startDate", {
+                    required: "Please select a start date!"
+                })} type={"date"} id={"startDate"}/><br/>
                 <label htmlFor={"type"}>Type</label><br/>
-                <select id={"type"} onChange={e => setCreatedContract({...createdContract, type: e.target.value})} value={createdContract.type}>
+                <select {...register("type")} id={"type"}>
                     <option>{TIME_AND_MATERIAL}</option>
                     <option>{FIXED_PRICE}</option>
                 </select><br/>
                 <label htmlFor={"budget"}>Budget amount (net)</label><br/>
-                <input type={"text"} id={"budget"} onChange={e => setCreatedContract({
-                    ...createdContract,
-                    budget: {amount: e.target.value, currencyCode: "EUR"}
-                })} value={createdContract.budget.amount}/><br/>
+                <input {...register("budget", {
+                    required: "Please enter a budget!"
+                })} type={"text"} id={"budget"}/><br/>
                 <label htmlFor={"taxRate"}>Tax Rate</label><br/>
-                <input type={"text"} id={"taxRate"}
-                       onChange={e => setCreatedContract({...createdContract, taxRate: e.target.value})} value={createdContract.taxRate}/><br/>
+                <input {...register("taxRate", {
+                    required: "Please enter a tax rate!"
+                })} type={"text"} id={"taxRate"}/><br/>
                 <br/>
                 <input type={"submit"} value={"Save"}/>
             </form>
