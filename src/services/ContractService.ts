5a7e329e017b47523e5a5f5812ae63795d0665b6
Maximilian Korte <maximilian.korte@adesso.de>
2022-03-09 13:47:26 +0100

    Implement useGet in ContractsPage

--- src/services/ContractService.ts
+++ src/services/ContractService.ts
@@ -2,8 +2,6 @@ import http from "../http-common";
 import Contract from "../types/Contract";
 import CreateContract from "../types/CreateContract";
 
-export const getContracts = (projectId: number) => http.get<Contract[]>(`/contracts?projectId=${projectId}`)
-
 export const getContract = (contractId: number) => http.get<Contract>(`/contracts/${contractId}`)
 
 export const createContract = (projectId: number, contract: CreateContract) => http.post<Contract>(`/contracts?projectId=${projectId}`, contract)
