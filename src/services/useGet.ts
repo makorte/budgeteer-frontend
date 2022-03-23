da07c1a0a6c9d18a0290cd0dc80cbeef72692e4c
Maximilian Korte <maximilian.korte@adesso.de>
2022-03-23 10:53:22 +0100

    Minor fix in useGet

--- src/services/useGet.ts
+++ src/services/useGet.ts
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 import http from "../http-common";
 import {useNavigate} from "react-router-dom";
 import {toLogin, toSelectProject} from "./NavigationService";
+import {AxiosError} from "axios";
 
 function useGet<T>(url: string, initialData: T, onError: string) {
     const [data, setData] = useState(initialData)
@@ -14,21 +15,35 @@ function useGet<T>(url: string, initialData: T, onError: string) {
 
         http.get(url)
             .then(res => setData(res.data))
-            .catch(err => {
-                if (err.response?.status === 401) {
-                    toLogin(navigate)
-                } else if (err.response?.status === 403) {
-                    toSelectProject(navigate)
-                } else if (err.response?.status === 400 || err.response?.status === 500) {
-                    navigate(onError)
-                } else {
-                    alert(err.message)
-                }
-            })
+            .catch(err => handleError(err))
             .finally(() => setLoading(false))
     }
 
-    useEffect(() => refetch(), [url])
+    useEffect(() => {
+        let isMounted = true
+        setLoading(true)
+
+        http.get(url)
+            .then(res => isMounted && setData(res.data))
+            .catch(err => handleError(err))
+            .finally(() => isMounted && setLoading(false))
+
+        return () => {
+            isMounted = false
+        }
+    }, [navigate, onError, url])
+
+    const handleError = (err: AxiosError) => {
+        if (err.response?.status === 401) {
+            toLogin(navigate)
+        } else if (err.response?.status === 403) {
+            toSelectProject(navigate)
+        } else if (err.response?.status === 400 || err.response?.status === 500) {
+            navigate(onError)
+        } else {
+            alert(err.message)
+        }
+    }
 
     return {data, loading, refetch}
 }
